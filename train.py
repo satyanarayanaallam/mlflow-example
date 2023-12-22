d967d7f007f223b09a4b77542ba88b0f26efa433
satyanarayanaallam <35998437+satyanarayanaallam@users.noreply.github.com>
2023-12-22 14:47:18 +0530

    Update train.py

--- train.py
+++ train.py
@@ -27,7 +27,7 @@ def eval_metrics(actual, pred):
 if __name__ == "__main__":
     warnings.filterwarnings("ignore")
     np.random.seed(40)
-
+    mlflow.set_tracking_uri("http://127.0.0.1:5000/")
     # Read the wine-quality csv file (make sure you're running this from the root of MLflow!)
     wine_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), "wine-quality.csv")
     data = pd.read_csv(wine_path)
